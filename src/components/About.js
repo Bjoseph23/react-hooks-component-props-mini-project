6e8d87a8e31952c3e73ddb4028cbc5805c7db33a
+ <+>
2024-04-29 11:26:37 +0300

    DOne

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-function About ({
-    image="https://via.placeholder.com/215", 
-    about,
-}){
+function About ({ image = "https://via.placeholder.com/215", about }){
     return(
         <aside>
-            <img src={image} alt="Blog logo" />
+            <img src={image.logo} alt="Blog logo" />
             <p>{about}</p>
         </aside>
     )
